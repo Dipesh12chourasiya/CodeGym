4a016f67e5f512c321926b0ca574d0c7dc60c75a
Dipesh <dipeshchourasiya12@gmail.com>
2024-02-17 12:15:36 +0530

    files addded

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hi");
+const express = require("express");
+
+const app = express();
+const PORT = 8000;
+
+app.get('/' , (req ,res)=>{
+    return res.send("Hi from the server");
+})
+
+app.listen(PORT , ()=> console.log("Server Started"));
