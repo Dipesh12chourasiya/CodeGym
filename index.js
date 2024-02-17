0292876098e3ee2a4633cb93c5e98c6a9d09ea5f
Dipesh <dipeshchourasiya12@gmail.com>
2024-02-17 14:37:19 +0530

    doneeee

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
+const path = require("path");
 const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+
+
+const userRoute = require("./routes/user");
+const { checkForAuthenticationCookie } = require("./middlewares/auth");
 
 const app = express();
 const PORT = 8000;
 
-app.get('/' , (req ,res)=>{
-    return res.send("Hi from the server");
-})
+//Data Base Connection
+mongoose.connect('mongodb://localhost:27017/GymCode').then(e =>
+    console.log("mongo connected"));
 
-app.listen(PORT , ()=> console.log("Server Started"));
+// Views
+app.set("view engine", "ejs");
+app.set("views", path.resolve('./views'));
+
+//MiddleWare
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(checkForAuthenticationCookie("token"));
+app.use(express.static(path.resolve("./public")));
+
+//Routes
+app.get('/',async (req, res) => {
+    res.render('home', {
+        user: req.user,
+    });
+});
+
+app.use('/user', userRoute);
+
+app.listen(PORT, () => console.log(`server started at ${PORT}`));
